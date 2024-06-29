eb98bb43105395bf75d9c620e44733eb56de02aa
paul tur <paultur@protonmail.com>
2024-06-29 15:52:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-    <h1>Blah Blah</h1>
+    <h1>WTF 202</h1>
     <h2>{count}</h2>
     <button onClick={() => setCount(prev=> prev + 1)}>Increment Count</button>
     <img src={logo}/>
